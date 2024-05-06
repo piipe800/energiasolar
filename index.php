7399c0cd4611815eb2dac7d503879223e9d37238
Ricardo León Sánchez <piipe_80@hotmail.com>
2024-05-05 21:33:29 -0500

    Se suben cambios del fontend

--- index.php
+++ index.php
@@ -1,57 +1,96 @@
 <!DOCTYPE html>
-<html>
+<html lang="es">
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Calculadora de Consumo de Energía</title>
+    <link rel="icon" href="img/Logo USTA.png" type="image/x-icon">
+    <link rel="shortcut icon" href="img/Logo USTA.png" type="image/x-icon">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/styles.css" rel="stylesheet">
 </head>
 <body>
-    <h1>Calculadora de Consumo de Energía</h1>
-    <form action="conversor.php" method="post" id="formulario">
-        <div id="lugar">
-            <label for="departamento">Departamento:</label>
-            <select id="departamento" name="departamento" onchange="cargarMunicipios()">
-                <option value="">Selecciona un departamento</option>
-                <?php foreach ($departamentos as $departamento): ?>
-                    <option value="<?php echo $departamento['departamento']; ?>"></option>
-                <?php endforeach; ?>
-            </select>
-            <br>
-            <label for="municipio">Municipio:</label>
-            <select id="municipio" name="municipio" disabled>
-                <option value="">Selecciona un municipio</option>
-                <!-- Aquí se cargarán los municipios según el departamento seleccionado -->
-            </select>
-            <br>
-            <label for="hsp">Horas de sol diarias:</label>
-            <input type="text" id="hsp" name="hsp" readonly>
-        </div>
-        <div id="equipos">
-            <div class="equipo">
-                <label for="nombre_equipo">Nombre del Equipo:</label>
-                <input type="text" name="nombre_equipo[]" required>
-                <label for="tiempo_uso">Tiempo de Uso al Día (horas):</label>
-                <input type="number" name="tiempo_uso[]" required>
-                <label for="consumo">Consumo (watts):</label>
-                <input type="number" name="consumo[]" required>
+    <div class="container-fluid bg-gradient">
+        <div class="row justify-content-center">
+            <div class="col-lg-8">
+                <div class="card">
+                    <div class="card-body">
+                        <h1 class="text-center display-4">Calculadora de Consumo de Energía</h1>
+                        <form action="conversor.php" method="post" id="formulario">
+                            <div class="row mb-3">
+                                <!-- Primer formulario -->
+                                <div class="col primer-formulario">
+                                    <label for="departamento">Departamento:</label>
+                                    <select id="departamento" name="departamento" onchange="cargarMunicipios()" class="form-select">
+                                        <option value="">Selecciona un departamento</option>
+                                        <?php foreach ($departamentos as $departamento): ?>
+                                            <option value="<?php echo $departamento['departamento']; ?>"><?php echo $departamento['departamento']; ?></option>
+                                        <?php endforeach; ?>
+                                    </select>
+                                </div>
+                                <!-- Segundo formulario -->
+                                <div class="col">
+                                    <label for="municipio">Municipio:</label>
+                                    <select id="municipio" name="municipio" disabled class="form-select">
+                                        <option value="">Selecciona un municipio</option>
+                                    </select>
+                                </div>
+                                <!-- Tercer formulario -->
+                                <div class="col">
+                                    <label for="hsp">Horas de sol diarias:</label>
+                                    <input type="text" id="hsp" name="hsp" readonly class="form-control">
+                                </div>
+                            </div>
+                            <!-- Formularios para los equipos -->
+                            <h2>Equipos en el Hogar</h2>
+                            <div id="equipos">
+                                <!-- Los primeros tres formularios -->
+                                <div class="row mb-3">
+                                    <div class="col">
+                                        
+                                        <label for="nombre_equipo">Nombre del Equipo:</label>
+                                        <input type="text" name="nombre_equipo[]" required class="form-control">
+                                    </div>
+                                    <div class="col">
+                                        <label for="tiempo_uso">Tiempo de Uso al Día (horas):</label>
+                                        <input type="number" name="tiempo_uso[]" required class="form-control">
+                                    </div>
+                                    <div class="col">
+                                        <label for="consumo">Consumo (watts):</label>
+                                        <input type="number" name="consumo[]" required class="form-control">
+                                    </div>
+                                </div>
+                            </div>
+                            <br>
+                            <button type="button" onclick="agregarEquipo()" class="btn btn-primary">Agregar Equipo</button>
+                            <input type="submit" value="Calcular Consumo" class="btn btn-success">
+                        </form>
+                    </div>
+                </div>
             </div>
         </div>
-        <button type="button" onclick="agregarEquipo()">Agregar Equipo</button>
-        <input type="submit" value="Calcular Consumo">
-    </form>
+    </div>
 
     <script>
         function agregarEquipo() {
             var equiposDiv = document.getElementById('equipos');
-            var nuevoEquipo = document.createElement('div');
-            nuevoEquipo.classList.add('equipo');
-            nuevoEquipo.innerHTML = `
-                <label for="nombre_equipo">Nombre del Equipo:</label>
-                <input type="text" name="nombre_equipo[]" required>
-                <label for="tiempo_uso">Tiempo de Uso al Día (horas):</label>
-                <input type="number" name="tiempo_uso[]" required>
-                <label for="consumo">Consumo (watts):</label>
-                <input type="number" name="consumo[]" required>
+            var nuevaFila = document.createElement('div');
+            nuevaFila.classList.add('row', 'mb-3');
+            nuevaFila.innerHTML = `
+                <div class="col">
+                    <label for="nombre_equipo">Nombre del Equipo:</label>
+                    <input type="text" name="nombre_equipo[]" required class="form-control">
+                </div>
+                <div class="col">
+                    <label for="tiempo_uso">Tiempo de Uso al Día (horas):</label>
+                    <input type="number" name="tiempo_uso[]" required class="form-control">
+                </div>
+                <div class="col">
+                    <label for="consumo">Consumo (watts):</label>
+                    <input type="number" name="consumo[]" required class="form-control">
+                </div>
             `;
-            equiposDiv.appendChild(nuevoEquipo);
+            equiposDiv.appendChild(nuevaFila);
         }
 
         function cargarMunicipios() {
@@ -131,7 +170,7 @@
                 }
             });
         });
-
     </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
